7676367e3798fcb1a0261123a66b007052a0168c
Hsiuan Ting Wang <49448790+htingwang@users.noreply.github.com>
2020-03-26 21:43:44 -0700

    Update Count-Complete-Tree-Nodes.py

--- LeetCode/0222.Count-Complete-Tree-Nodes/Count-Complete-Tree-Nodes.py
+++ LeetCode/0222.Count-Complete-Tree-Nodes/Count-Complete-Tree-Nodes.py
@@ -1,39 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def countNodes(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        return self.countNodes2(root)
+    
+    def countNodes2(self, root):
         if not root: return 0
-        
+        d = 0
         node = root
-        depth = 0
         while node.left:
+            d += 1
             node = node.left
-            depth += 1
-        
-        if depth == 0: return 1
-        
-        left, right = 1, 2 ** depth - 1
-        
-        while left <= right:
-            #print(left, right, depth)
-            mid = left + (right - left) // 2
-            if self.exist(root, mid, depth):
-                left = mid + 1
+        left, right = 0, 2 ** d - 1
+        while left < right:
+            mid = (left + right + 1) // 2
+            if self.exist(root, d, mid):
+                left = mid
             else:
                 right = mid - 1
-                
-        return 2 ** depth - 1 + left
+        return 2 ** d + left
     
-    def exist(self, node, value, depth):
-        left, right = 0, 2 ** depth - 1
-        for _ in range(depth):
-            mid = left + (right - left) // 2
-            if value <= mid:
-                node = node.left
+    def exist(self, node, d, idx):
+        left, right = 0, 2 ** d - 1
+        for _ in range(d):
+            mid = (left + right) // 2
+            if mid >= idx:
                 right = mid
+                node = node.left
             else:
-                node = node.right
                 left = mid + 1
-        return node is not None    
+                node = node.right
+        return node != None
+                
+    def countNodes1(self, root):
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1 if root else 0
