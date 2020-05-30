f04b6b2c35306b0f95191995139997e8c9ca86dd
htingwang <puggywang@gmail.com>
2020-05-29 23:44:57 -0700

    Update Count-Of-Smaller-Numbers-After-Self.py

--- LeetCode/0315.Count-of-Smaller-Numbers-After-Self/Count-Of-Smaller-Numbers-After-Self.py
+++ LeetCode/0315.Count-of-Smaller-Numbers-After-Self/Count-Of-Smaller-Numbers-After-Self.py
@@ -4,6 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        return self.countSmaller1(nums)
+    
+    # Binary index tree. Time: O(NlogN)
+    def countSmaller1(self, nums):
+        rank = {num: i + 1 for i, num in enumerate(sorted(nums))} 
+        n = len(nums)
+        bit = [0] * (n + 1)
+        
+        def update(i, val):
+            while i < len(bit):
+                bit[i] += val
+                i += (i & -i)
+                
+        def query(i):
+            res = 0
+            while i > 0:
+                res += bit[i]
+                i -= (i & -i)
+            return res
+                
+        res = []
+        for num in reversed(nums):
+            res.append(query(rank[num] - 1))
+            update(rank[num], 1)
+            
+        return res[::-1]
+    
+    # Binary Search. Time: O(N^2) because insort takes O(N)
+    def countSmaller2(self, nums):
         arr = []
         res = [0] * len(nums)
         for i in range(len(nums) - 1, -1, -1):
