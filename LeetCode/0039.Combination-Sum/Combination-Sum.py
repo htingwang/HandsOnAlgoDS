8ac96ea37aece0abecc14ab07081e4c9a5c1e1ff
htingwang <puggywang@gmail.com>
2020-06-18 20:12:23 -0700

    Update Combination-Sum.py

--- LeetCode/0039.Combination-Sum/Combination-Sum.py
+++ LeetCode/0039.Combination-Sum/Combination-Sum.py
@@ -1,13 +1,20 @@
 class Solution(object):
-    def combinationSum(self, candidates, target):	
-        #print [candidates, target]
-        res = [];
-        for i in range(len(candidates)):
-            ans = [candidates[i]];
-            if target == candidates[i]:
-                res.append(ans);
-            elif target > candidates[i]:
-                subans = self.combinationSum(candidates[i:],target-candidates[i]);
-                for i in subans: 
-                    res.append(ans+i);
-        return res;
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        
+        for i, num in enumerate(candidates):
+            ans = [num]
+            if num == target:
+                res.append(ans)
+            if num < target:
+                subans = self.combinationSum(candidates[i:], target - num)
+                for sub in subans:
+                    res.append(ans + sub)
+            
+        return res
+        
