c96fc5f2d25b5a8d948e1c2a2cfa5a16b4417167
htingwang <puggywang@gmail.com>
2020-07-03 19:56:49 -0700

    Update Container-With-Most-Water.py

--- LeetCode/0011.Container-With-Most-Water/Container-With-Most-Water.py
+++ LeetCode/0011.Container-With-Most-Water/Container-With-Most-Water.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l, r = 0, len(height)-1;
-        ans = 0;
+        l, r = 0, len(height) - 1
+        res = 0
         while l < r:
-            ans = max(ans, min(height[l], height[r])*(r-l));
-            if height[l] < height[r]:
-                l += 1;
-            else:
-                r -= 1;
-        return ans;
-        
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]: l += 1
+            else: r -= 1
+        return res
+    
