125b7a2bd28e57f0a58ddbaecce6d7efa9cbedd3
htingwang <puggywang@gmail.com>
2020-07-03 19:56:32 -0700

    Update Trapping-Rain-Water-Ii.py

--- LeetCode/0407.Trapping-Rain-Water-Ii/Trapping-Rain-Water-Ii.py
+++ LeetCode/0407.Trapping-Rain-Water-Ii/Trapping-Rain-Water-Ii.py
@@ -6,33 +6,22 @@ class Solution(object):
         :rtype: int
         """
         if not heightMap or not heightMap[0]: return 0
-        
         minheap = []
-        visited = set()
-        
+        seen = set()
         m, n = len(heightMap), len(heightMap[0])
-        
         for i in range(m):
-            heapq.heappush(minheap, (heightMap[i][0], i, 0))
-            heapq.heappush(minheap, (heightMap[i][n - 1], i, n - 1))
-            visited.add((i, 0))
-            visited.add((i, n - 1))
-            
-        for j in range(1, n - 1):
-            heapq.heappush(minheap, (heightMap[0][j], 0, j))
-            heapq.heappush(minheap, (heightMap[m - 1][j], m - 1, j))
-            visited.add((0, j))
-            visited.add((m - 1, j))
-        
+            for j in range(n):
+                if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                    heapq.heappush(minheap, (heightMap[i][j], i, j)) 
+                    seen.add((i, j))
         res = 0
         while minheap:
-            h, x, y = heapq.heappop(minheap)
-            for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and (nx, ny) not in visited:
-                    res += max(0, h - heightMap[nx][ny])
-                    #print(nx, ny, h - heightMap[nx][ny])
-                    heapq.heappush(minheap, (max(h, heightMap[nx][ny]), nx, ny))
-                    visited.add((nx, ny))
+            h, i, j = heapq.heappop(minheap)
+            for ni, nj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if  0 <= ni < m and 0  <= nj < n and (ni, nj) not in seen:
+                    res += max(0, h - heightMap[ni][nj])
+                    heapq.heappush(minheap, (max(h, heightMap[ni][nj]), ni, nj))
+                    seen.add((ni, nj))
         return res
+
                     
