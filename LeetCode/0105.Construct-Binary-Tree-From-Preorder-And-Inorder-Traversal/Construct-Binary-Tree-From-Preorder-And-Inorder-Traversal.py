256df31fb53d4d53afe7f5f5924e14851ddb4a00
htingwang <puggywang@gmail.com>
2020-07-04 17:00:07 -0700

    Update Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py

--- LeetCode/0105.Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal/Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py
+++ LeetCode/0105.Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal/Construct-Binary-Tree-From-Preorder-And-Inorder-Traversal.py
@@ -12,6 +12,27 @@ class Solution(object):
         :type inorder: List[int]
         :rtype: TreeNode
         """
+        return self.buildTree2(preorder, inorder)
+    
+    def buildTree2(self, preorder, inorder):
+        
+        pre = collections.deque(preorder)
+        toidx = {val: i for i, val in enumerate(inorder)}
+        n = len(inorder)
+        
+        def dfs(s, e):
+            if s > e: return None
+            
+            val = pre.popleft()
+            root = TreeNode(val)
+            root.left = dfs(s, toidx[val] - 1)
+            root.right = dfs(toidx[val] + 1, e)
+            
+            return root
+            
+        return dfs(0, n - 1)
+        
+    def buildTree1(self, preorder, inorder):
         if not preorder: return None;
         root = TreeNode(preorder[0]);
         stack = [root];
