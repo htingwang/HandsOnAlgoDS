5b14802f372622b04ff72df24b87522b038eaddc
htingwang <puggywang@gmail.com>
2020-07-05 17:52:06 -0700

    Create Can-Make-Arithmetic-Progression-From-Sequence.py

---
+++ LeetCode/1502.Can-Make-Arithmetic-Progression-From-Sequence/Can-Make-Arithmetic-Progression-From-Sequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(2, len(arr)):
+            if arr[i] - arr[i - 1] != diff: return False
+        return True
+        
