d66d7559aa797e28e9131d5c197a626597c9d206
htingwang <puggywang@gmail.com>
2020-07-08 23:13:05 -0700

    Update Reverse-Words-In-A-String.py

--- LeetCode/0151.Reverse-Words-in-a-String/Reverse-Words-In-A-String.py
+++ LeetCode/0151.Reverse-Words-in-a-String/Reverse-Words-In-A-String.py
@@ -4,30 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        s = s[::-1]
-        index = i = 0
-        #print(s)
-        while i < len(s):
-            if s[i] != " ":
-                #print(i, index)
-                if index != 0:
-                    s = s[:index] + " " + s[index + 1:]
-                    index += 1
-                    #print(s, index)
-                    #i += 1
-                j = i
-                tmp = ""
-                while j < len(s) and s[j] != " ":
-                    tmp = s[j] + tmp
-                    j += 1 
-                #print(index, tmp, j - i)
-                s = s[:index] + tmp + s[index + j - i: ]
-                #print(s, i, j)
-                index += (j - i)
-                i = j
-            else:
-                i += 1
-        s = s[:index]
-        #print(s, index, i, j)
-        return s
-                
+        return self.reverseWords2(s)
+    
+    def reverseWords2(self, s):
+        return " ".join(reversed(s.split()))
+        
+        
+    def reverseWords1(self, s):
+        s = s.strip(' ')
+        queue = collections.deque()
+        left = 0
+        for i, c in enumerate(s):
+            if i > 0 and s[i - 1] == ' ': left = i
+            if i == len(s) - 1 or (s[i + 1] == ' ' and s[i] != ' '):
+                queue.appendleft(s[left : i + 1])
+            
+        return " ".join(queue)
