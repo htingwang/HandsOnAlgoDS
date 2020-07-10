a8a42edccec94d09aa0f619683630335123eb0b2
htingwang <puggywang@gmail.com>
2020-07-09 17:08:00 -0700

    Update Best-Time-To-Buy-And-Sell-Stock.py

--- LeetCode/0121.Best-Time-To-Buy-And-Sell-Stock/Best-Time-To-Buy-And-Sell-Stock.py
+++ LeetCode/0121.Best-Time-To-Buy-And-Sell-Stock/Best-Time-To-Buy-And-Sell-Stock.py
@@ -1,11 +1,15 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_profix = 0
-        min_price = float('inf')
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profix:
-                max_profix = prices[i] - min_price
-
-        return max_profix
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n = len(prices)
+        if not n: return 0
+        mn = prices[0]
+        res = 0
+        for i in range(1, n):
+            res = max(res, prices[i] - mn)
+            mn = min(mn, prices[i])
+        return max(0, res)
+        
