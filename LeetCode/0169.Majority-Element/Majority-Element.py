ae14d78e980b800ffa0ddfaaf88df6e4dbc466f1
htingwang <puggywang@gmail.com>
2020-07-13 22:28:27 -0700

    Update Majority-Element.py

--- LeetCode/0169.Majority-Element/Majority-Element.py
+++ LeetCode/0169.Majority-Element/Majority-Element.py
@@ -4,6 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort();
-        return(nums[len(nums)/2]);
+        return self.majorityElement2(nums)
+    
+    def majorityElement2(self, nums):
+        res = cnt = 0
+        for num in nums:
+            if cnt == 0: res = num
+            if num == res: cnt += 1
+            else: cnt -= 1
+        return res
         
+    def majorityElement1(self, nums):
+        nums.sort();
+        return(nums[len(nums)/2]);        
